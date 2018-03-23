0114e4566548b6dc19b6db2760c7d962a24ed658
uvuvuevuevue <8========D--->
2018-03-23 10:23:54 +0100

    Added a function Now you can pass an argument and that will be greeted

--- Hello.java
+++ Hello.java
@@ -1,8 +1,13 @@
 public class Hello {
 
     public static void main(String[] args) {
+	if (args.length > 0 && args[0]!= null)
+	// Prints "Hello ~first argument~!" to the terminal window.
+		System.out.println("Hello " + args[0] + "!");
+	else
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+	
     }
 
 }
